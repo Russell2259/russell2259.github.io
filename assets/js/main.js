7303a214ba0b9965d90ec48d536d2d4b2deb796d
Russell2259 <84256826+Russell2259@users.noreply.github.com>
2023-01-25 09:18:11 -0700

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,6 @@ if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
     document.documentElement.setAttribute('data-theme', 'light');
 }
 
-const svg = document.querySelectorAll('path')[1];
-svg.fill = getComputedStyle(document.documentElement).getPropertyValue('--wave-color');
-alert(svg.fill);
+/*const svg = document.querySelectorAll('path')[1];
+svg.fill = getComputedStyle(document.documentElement).getPropertyValue('--wave-color');*/
+alert('hi');
