4c1d3e17b9f1954985d8ecf7a2753fdab93c3f83
Russell2259 <84256826+Russell2259@users.noreply.github.com>
2023-01-25 09:07:51 -0700

    dark/light theme test

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+} else {
+    document.documentElement.setAttribute('data-theme', 'light');
+}
+
+const svg = document.querySelectorAll('path')[1];
+svg.fill = getComputedStyle(document.documentElement).getPropertyValue('--wave-color');
