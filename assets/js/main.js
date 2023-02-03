46fbea5928a7187f7525b9bbbf64c0a2818a5db0
Russell2259 <kidskrebs@gmail.com>
2023-02-02 20:21:06 -0700

    update css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+window.scrollTo(0, 0);
+
 onerror = (e) => {
     alert(e);
 }
@@ -6,4 +8,15 @@ if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
     document.documentElement.setAttribute('data-theme', 'dark');
 } else {
     document.documentElement.setAttribute('data-theme', 'light');
+}
+
+var slide = 0;
+const slideEl = document.querySelector('#slide');
+
+function scrollSlide(direction) {
+    if (direction === 'left') {
+        slideEl.scrollTo(slideEl.scrollLeft -= document.querySelector(`.item[data-slideid="${slide}"]`).clientWidth, 0);
+    } else if (direction === 'right') {
+        slideEl.scrollTo(slideEl.scrollLeft += document.querySelector(`.item[data-slideid="${slide}"]`).clientWidth, 0);
+    }
 }
