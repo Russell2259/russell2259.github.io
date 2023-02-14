d214ffcaa2bf4bde34cae85a81d675da5e21aa20
Russell2259 <russell2259@tuta.io>
2023-02-13 23:29:20 -0700

    wip

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,6 +21,6 @@ function scrollSlide(direction) {
     }
 }
 
-const tabmang = document.createElement('script');
+/*const tabmang = document.createElement('script');
 tabmang.src = '/assets/js/tab.js';
-document.body.appendChild(tabmang);
+document.body.appendChild(tabmang);*/
---
+++ assets/js/slide.js
@@ -0,0 +1,35 @@
+let slides = [];
+
+function loadSlide(slideData, slideElement) {
+    const slideShowId = Math.floor(Math.random() * 7);
+    const slides = [];
+
+    slideElement.innerHTML = `<div class="slideshow_container">
+        <span class="arrow">
+            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+                <path fill="currentColor"
+                   d="M14.71 6.71a.996.996 0 0 0-1.41 0L8.71 11.3a.996.996 0 0 0 0 1.41l4.59 4.59a.996.996 0 1 0 1.41-1.41L10.83 12l3.88-3.88c.39-.39.38-1.03 0-1.41z" />
+            </svg>
+        </span>
+
+        <div class="slides" data-slideid="${slideShowId}"></div>
+
+        <span class="arrow" data-position="right">
+            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+                <path fill="currentColor"
+                    d="M9.29 6.71a.996.996 0 0 0 0 1.41L13.17 12l-3.88 3.88a.996.996 0 1 0 1.41 1.41l4.59-4.59a.996.996 0 0 0 0-1.41L10.7 6.7c-.38-.38-1.02-.38-1.41.01z" />
+            </svg>
+        </span>
+    </div>`;
+
+    slideData.forEach(slide => {
+        slides.push(`<div class="item">
+            <h1>${slide.title}</h1>
+            <div class="slide_content">${slide.content}</div>
+        </div>`);
+    });
+
+    const slidesElement = document.querySelector(`[data-slideid="${slideShowId}"]`);
+
+    return slideShowId;
+}
