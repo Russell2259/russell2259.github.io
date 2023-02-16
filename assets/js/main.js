3423a01ca5b52a546a6196861494b11a5d0147fc
Russell2259 <russell2259@tuta.io>
2023-02-16 09:15:16 -0700

    try and fix tab.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
+const tabmang = document.createElement('script');
+tabmang.src = '/assets/js/tab.js';
+document.body.appendChild(tabmang);
+
 window.scrollTo(0, 0);
 
 window.onerror = (e) => {
@@ -9,7 +13,3 @@ if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
 } else {
     document.documentElement.setAttribute('data-theme', 'light');
 }
-
-const tabmang = document.createElement('script');
-tabmang.src = '/assets/js/tab.js';
-document.body.appendChild(tabmang);
