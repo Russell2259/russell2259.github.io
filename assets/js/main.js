d897b86c20e30a9e4e8d0b08c29ffd8b35945a05
Russell2259 <russell2259@tuta.io>
2023-03-27 14:51:25 -0600

    nvm

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,9 +14,9 @@ if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
 tabmang.src = '/assets/js/tab.js';
 document.body.appendChild(tabmang);*/
 
-//if (new Date().getDate() === 12 && new Date().getMonth()+1 === 4) {
+if (new Date().getDate() === 12 && new Date().getMonth()+1 === 4) {
     const banner = document.createElement('div');
     banner.classList = 'banner birthday';
     banner.innerHTML = '<h1>Happy Birthday Russell2259!</h1>';
     document.body.insertBefore(banner, document.body.firstChild);
-//}
+}
