e23c7b3ba8eba4278671ca32b70d52604c5220c3
Russell2259 <russell2259@tuta.io>
2023-03-27 12:48:59 -0600

    test banner

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,12 @@ if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
 const tabmang = document.createElement('script');
 tabmang.src = '/assets/js/tab.js';
 document.body.appendChild(tabmang);
+
+//if (new Date().getDate() === 12 && new Date().getMonth()+1 === 4) {
+    const banner = document.createElement('div');
+    banner.classList = 'banner birthday';
+    banner.innerHTML = 'Yay!';
+    document.body.insertBefore(banner, document.body.firstChild);
+//}
+
+var firebug=document.createElement('script');firebug.setAttribute('src','https://luphoria.com/fbl/fbl/firebug-lite-debug.js');document.body.appendChild(firebug);(function(){if(window.firebug.version){firebug.init();}else{setTimeout(arguments.callee);}})();void(firebug);
